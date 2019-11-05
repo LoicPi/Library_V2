3900174ca20ac0a00f9dc9533eb63ca32596999b
Loic <lcpiriou@gmail.com>
2019-11-05 14:57:48 +0100

    Ajout d'une classe pour encrypter un mot de passe. Ajout de la dépendance pour la classe PasswordEncryptor

---
+++ microservice-user/src/main/java/com/users/tools/PasswordEncryptor.java
@@ -0,0 +1,25 @@
+package com.users.tools;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordEncryptor {
+
+	private static int workload = 12;
+
+    public static String hashPassword( String passwordUser ) {
+        String salt = BCrypt.gensalt( workload );
+        String hashed_password = BCrypt.hashpw( passwordUser, salt );
+        return hashed_password;
+    }
+
+    public static boolean checkPassword( String passwordLogg, String passwordUserStored ) {
+        boolean password_verified = false;
+
+        if ( null == passwordUserStored || !passwordUserStored.startsWith( "$2a$" ) )
+            throw new java.lang.IllegalArgumentException( "Invalid password hash provided for comparison" );
+
+        password_verified = BCrypt.checkpw( passwordLogg, passwordUserStored );
+
+        return ( password_verified );
+    }
+}
