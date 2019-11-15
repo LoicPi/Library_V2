b5bb332e3ae2725867d713143b366ac9312f9d68
Loic <lcpiriou@gmail.com>
2019-11-15 15:01:53 +0100

    Suppression d'une fonction de recherche

--- microservice-user/src/main/java/com/users/dao/UserDao.java
+++ microservice-user/src/main/java/com/users/dao/UserDao.java
@@ -1,7 +1,5 @@
 package com.users.dao;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,11 +8,7 @@ import com.users.model.User;
 @Repository
 public interface UserDao extends JpaRepository<User, Integer> {
 
-	User findByEmail(String email);
+	User findByemail(String email);
 	User findUserById(int id);
-	
-	Optional<User> findUserWithEmail(String email);
-	
-	
-	
+
 }
