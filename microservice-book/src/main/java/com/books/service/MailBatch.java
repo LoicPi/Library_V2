6ee0021e034add452d88ee3c995b3f020a046603
Loic <lcpiriou@gmail.com>
2020-02-06 13:05:22 +0100

    Ajout d'un batch pour vérifier si une réservation est au dela de 48h pour la supprimer et envoyer un mail à la personne suivante dans la liste de reservation.

--- microservice-book/src/main/java/com/books/service/MailBatch.java
+++ microservice-book/src/main/java/com/books/service/MailBatch.java
@@ -3,23 +3,35 @@ package com.books.service;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.books.beans.UserBean;
+import com.books.dao.BookingDao;
 import com.books.dao.BorrowingDao;
+import com.books.model.Book;
+import com.books.model.Booking;
 import com.books.model.Borrowing;
 import com.books.model.State;
 import com.books.proxies.MicroserviceUserProxy;
+import com.books.web.controller.BookingController;
 
 @Component
 public class MailBatch {
 	
 	@Autowired
 	private BorrowingDao borrowingDao;
+	
+	@Autowired
+	private BookingDao bookingDao;
+	
+	@Autowired
+	private BookingController bookingController;
 	
 	@Autowired
 	private MicroserviceUserProxy microserviceUserProxy;
@@ -73,6 +85,49 @@ public class MailBatch {
                     "\n\nL'équipe de la Bibliothèque";
             mailService.sendMessage( mailTo, mailSubject, mailText );
 		}	
+	}
+	
+	@Scheduled(cron = "*/60 * * * * *")
+	public void sendBookingMail () {
+		
+		List<Booking> bookingList = bookingDao.findBookingBySendMail(true);
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			
+		for (Booking booking : bookingList) {
+			
+			Date dateDeadline = DateUtils.addDays(booking.getDateMail(), 2);
+			
+			if(booking.getDateMail().after(dateDeadline)) {
+				
+				Integer bookId = booking.getBook().getId();
+				
+				bookingController.delBooking(booking.getId());
+				
+				Booking bookingMail = bookingDao.findByBookAndPosition(bookId, 1);
+				
+				Book book = bookingMail.getBook();
+				
+				Date deadlineBooking = DateUtils.addDays(bookingMail.getDateMail(), 2);
+				
+				UserBean user = microserviceUserProxy.getUser(bookingMail.getIdUser());
+				
+				String mailTo = user.getEmail();
+				String mailSubject = "Bibliothèque : Votre réservation est disponible";
+				String mailText = "Bonjour " + user.getFirstName() + " " + user.getLastName() + "," +
+						"\n\nNous vous informons que la réservation du document ci-dessous est disponible : " +
+						"\n\n" + book.getName() +
+						"\n\nVous avez jusqu'au " + dateFormat.format(deadlineBooking) + " pour venir récupérer votre document." +
+						"\n\nPassé cette date, le document sera remis en circulation." +
+						"\n\nCordialement," +
+	                    "\n\nL'équipe de la Bibliothèque";
+				
+				mailService.sendMessage( mailTo, mailSubject, mailText );
+			}
+		}
+		
+		
+		
 	}
 
 }
