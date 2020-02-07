c5781d3ea29b27306da2d224110dc474607a2101
Loic <lcpiriou@gmail.com>
2020-02-07 12:26:05 +0100

    Mise en place du correctif Lancement d'un batch passant tous les emprunts renouvelés ou non au statut en retard dès que la date de retour est dépassé dans le microservice-book. Dans la vue utilisateur, le bouton ne s'affiche plus si le statut de l'emprunt est "En retard".

--- microservice-book/src/main/java/com/books/service/BorrowingBatch.java
+++ microservice-book/src/main/java/com/books/service/BorrowingBatch.java
@@ -12,6 +12,9 @@ import com.books.dao.BorrowingDao;
 import com.books.model.Borrowing;
 import com.books.model.State;
 
+//#Issues 2 Correction de bug :
+//Il ne doit pas être possible pour l’usager de prolonger un prêt si la date de fin de prêt est dépassée.
+//Création d'un batch passant les emprunts dépassant la date du jour au statut en retard
 @Component
 public class BorrowingBatch {
 	
