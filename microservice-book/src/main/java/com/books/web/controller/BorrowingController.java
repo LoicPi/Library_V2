55cef6f4bc037d9c6ae7ed178fa3a0cac902a6d9
Loic <lcpiriou@gmail.com>
2020-03-04 11:54:33 +0100

    Correction de bug sur la classe controller de borrowing Vérification que la date du jour n'est pas après la date de fin de prêt sinon une exception est levée.

--- microservice-book/src/main/java/com/books/web/controller/BorrowingController.java
+++ microservice-book/src/main/java/com/books/web/controller/BorrowingController.java
@@ -39,230 +39,247 @@ import com.books.service.MailService;
 
 @RestController
 public class BorrowingController {
-	
+
 	Logger log = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private BorrowingDao borrowingDao;
-	
+
 	@Autowired
 	private BookingDao bookingDao;
-	
+
 	@Autowired
 	private MicroserviceUserProxy microserviceUserProxy;
-	
+
 	@Autowired
 	private MailService mailService;
-	
+
 	@Autowired
 	private BookingService bookingService;
-	
+
 	/**
 	 * Function to get a list of borrowing
+	 * 
 	 * @return a list of borrowing
 	 */
 	@GetMapping("/emprunts")
 	public List<Borrowing> listBorrowings() {
-		
+
 		List<Borrowing> borrowings = borrowingDao.findAll();
-		
-		if(borrowings.isEmpty()) throw new BorrowingNotFoundException("Les emprunts de livres n'ont pas été retrouvés.");
-		
+
+		if (borrowings.isEmpty())
+			throw new BorrowingNotFoundException("Les emprunts de livres n'ont pas été retrouvés.");
+
 		log.info("Récupération de la liste des emprunts des livres");
-		
+
 		return borrowings;
 	}
-	
+
 	/**
 	 * Function to get the user's list of borrowing
+	 * 
 	 * @param idUser id of user who wants have is borrowing
 	 * @return list of borrowing
 	 */
 	@GetMapping("/emprunts/utilisateur/{idUser}")
 	public List<Borrowing> listBorrowingsOfUser(@PathVariable int idUser) {
-		
+
 		List<Borrowing> borrowingsOfUser = borrowingDao.findByIdUser(idUser);
-		
+
 		log.info("Récupération de la liste des emprunts d'un utilisateur");
-		
+
 		return borrowingsOfUser;
 	}
-	
+
 	/**
 	 * Function to add borrowing
-	 * @param borrowing The object borrowing to add 
+	 * 
+	 * @param borrowing The object borrowing to add
 	 * @return response entity
 	 */
 	@PostMapping("/emprunt/add-borrowing")
-	public ResponseEntity<Borrowing> addBorrowing (@Valid @RequestBody Borrowing borrowing) {
-		
+	public ResponseEntity<Borrowing> addBorrowing(@Valid @RequestBody Borrowing borrowing) {
+
 		LocalDate localDate = LocalDate.now();
-		
+
 		Book bookBorrowed = borrowing.getBookCopy().getBook();
-		
+
 		Integer borrowingIdUser = borrowing.getIdUser();
-		
-		List<Booking> bookingBook = bookingDao.findBookingByBookAndStateOrderByDateCreate(bookBorrowed, State.EnAttente);
-		
+
+		List<Booking> bookingBook = bookingDao.findBookingByBookAndStateOrderByDateCreate(bookBorrowed,
+				State.EnAttente);
+
 		if (bookingBook.size() > 0) {
 			Booking booking = bookingBook.get(0);
-			
+
 			Integer bookingIdUser = booking.getIdUser();
-			
-			if(borrowingIdUser.equals(bookingIdUser)) {
+
+			if (borrowingIdUser.equals(bookingIdUser)) {
 				borrowing.setDateBorrowed(java.sql.Date.valueOf(localDate));
-				
+
 				borrowing.setDeadline(java.sql.Date.valueOf(localDate.plusMonths(1)));
-				
+
 				borrowing.setState(State.EnCours);
-				
-				LocalDate ld = LocalDate.of( 2001 , 01 , 01 ) ; 
-				
+
+				LocalDate ld = LocalDate.of(2001, 01, 01);
+
 				borrowing.setDateRenewal(java.sql.Date.valueOf(ld));
-				
+
 				borrowing.setDateReturn(java.sql.Date.valueOf(ld));
-				
+
 				Borrowing newBorrowing = borrowingDao.save(borrowing);
-				
+
 				bookingService.endBooking(booking.getId());
-				
-				if(newBorrowing == null) throw new CanNotAddBorrowingException ("Impossible d'ajouter l'emprunt.");
-				
+
+				if (newBorrowing == null)
+					throw new CanNotAddBorrowingException("Impossible d'ajouter l'emprunt.");
+
 				return new ResponseEntity<Borrowing>(newBorrowing, HttpStatus.OK);
 			} else {
-				throw new CanNotAddBorrowingException ("Ce livre est reservé par un autre utilisateur.");
+				throw new CanNotAddBorrowingException("Ce livre est reservé par un autre utilisateur.");
 			}
 		} else {
 			borrowing.setDateBorrowed(java.sql.Date.valueOf(localDate));
-			
+
 			borrowing.setDeadline(java.sql.Date.valueOf(localDate.plusMonths(1)));
-			
+
 			borrowing.setState(State.EnCours);
-			
-			LocalDate ld = LocalDate.of( 2001 , 01 , 01 ) ; 
-			
+
+			LocalDate ld = LocalDate.of(2001, 01, 01);
+
 			borrowing.setDateRenewal(java.sql.Date.valueOf(ld));
-			
+
 			borrowing.setDateReturn(java.sql.Date.valueOf(ld));
-			
+
 			Borrowing newBorrowing = borrowingDao.save(borrowing);
-			
-			if(newBorrowing == null) throw new CanNotAddBorrowingException ("Impossible d'ajouter l'emprunt.");
-			
+
+			if (newBorrowing == null)
+				throw new CanNotAddBorrowingException("Impossible d'ajouter l'emprunt.");
+
 			return new ResponseEntity<Borrowing>(newBorrowing, HttpStatus.OK);
-		}			
+		}
 	}
-	
+
 	/**
 	 * Function to renewal borrowing
+	 * 
 	 * @param id the id of borrowing
 	 * @return response entity
 	 */
 	@PostMapping("/emprunt/{id}/renouvellement")
-	public ResponseEntity<Borrowing> renewalBorrowing (@PathVariable int id)  {
-		
+	public ResponseEntity<Borrowing> renewalBorrowing(@PathVariable int id) {
+
 		Optional<Borrowing> borrowing = borrowingDao.findById(id);
-		
-		if(!borrowing.isPresent()) throw new BorrowingNotFoundException("L'emprunt avec l'id : " + id + " n'a pas été retrouvé.");
-		
+
+		if (!borrowing.isPresent())
+			throw new BorrowingNotFoundException("L'emprunt avec l'id : " + id + " n'a pas été retrouvé.");
+
 		Borrowing borrowingRenewal = borrowing.get();
-		
-		if(borrowingRenewal.getRenewal() == true) {
-			throw new BorrowingInvalidRenewalException ("Borrowing01");
+
+		if (borrowingRenewal.getRenewal() == true) {
+			throw new BorrowingInvalidRenewalException("Borrowing01");
 		}
-		
+
+		Date today = new Date();
+
+		if (today.after(borrowingRenewal.getDeadline())) {
+			throw new BorrowingInvalidRenewalException("Borrowing02");
+		}
+
 		borrowingRenewal.setRenewal(true);
-		
+
 		LocalDate localDate = LocalDate.now();
-		
+
 		borrowingRenewal.setDateRenewal(java.sql.Date.valueOf(localDate));
-		
+
 		Date newDeadline = DateUtils.addMonths(borrowingRenewal.getDeadline(), 1);
-		
+
 		borrowingRenewal.setDeadline(newDeadline);
-		
+
 		borrowingRenewal.setState(State.Renouvele);
-		
+
 		Borrowing borrowingRenew = borrowingDao.save(borrowingRenewal);
-		
+
 		return new ResponseEntity<Borrowing>(borrowingRenew, HttpStatus.OK);
 	}
-	
+
 	/**
 	 * Function to close a borrowing
+	 * 
 	 * @param id id the borrowing
 	 * @return response entity
 	 */
 	@PostMapping("/emprunt/{id}/returnborrowing")
 	public ResponseEntity<Borrowing> returnBorrowing(@PathVariable int id) {
-		
+
 		Optional<Borrowing> borrowing = borrowingDao.findById(id);
-		
-		if(!borrowing.isPresent()) throw new BorrowingNotFoundException("L'emprunt avec l'id : " + id + " n'a pas été retrouvé.");
-		
+
+		if (!borrowing.isPresent())
+			throw new BorrowingNotFoundException("L'emprunt avec l'id : " + id + " n'a pas été retrouvé.");
+
 		Borrowing borrowingReturn = borrowing.get();
-		
+
 		LocalDate localDate = LocalDate.now();
-		
+
 		borrowingReturn.setDateReturn(java.sql.Date.valueOf(localDate));
-		
+
 		borrowingReturn.setState(State.Termine);
-		
+
 		Book bookReturn = borrowingReturn.getBookCopy().getBook();
-		
+
 		List<Booking> bookings = bookingDao.findBookingByBookAndStateOrderByDateCreate(bookReturn, State.EnAttente);
-		
+
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		if (bookings.size() > 0) {
-			
+
 			Booking booking = bookings.get(0);
-			
+
 			booking.setDateMail(java.sql.Date.valueOf(localDate));
-			
+
 			Date deadlineBooking = DateUtils.addDays(booking.getDateMail(), 2);
-			
+
 			UserBean user = microserviceUserProxy.getUser(booking.getIdUser());
-			
+
 			String mailTo = user.getEmail();
 			String mailSubject = "Bibliothèque : Votre réservation est disponible";
-			String mailText = "Bonjour " + user.getFirstName() + " " + user.getLastName() + "," +
-					"\n\nNous vous informons que la réservation du document ci-dessous est disponible : " +
-					"\n\n" + bookReturn.getName() +
-					"\n\nVous avez jusqu'au " + dateFormat.format(deadlineBooking) + " pour venir récupérer votre document." +
-					"\n\nPassée cette date, le document sera remis en circulation." +
-					"\n\nCordialement," +
-                    "\n\nL'équipe de la Bibliothèque";
-			
-			mailService.sendMessage( mailTo, mailSubject, mailText );
-			
+			String mailText = "Bonjour " + user.getFirstName() + " " + user.getLastName() + ","
+					+ "\n\nNous vous informons que la réservation du document ci-dessous est disponible : " + "\n\n"
+					+ bookReturn.getName() + "\n\nVous avez jusqu'au " + dateFormat.format(deadlineBooking)
+					+ " pour venir récupérer votre document."
+					+ "\n\nPassée cette date, le document sera remis en circulation." + "\n\nCordialement,"
+					+ "\n\nL'équipe de la Bibliothèque";
+
+			mailService.sendMessage(mailTo, mailSubject, mailText);
+
 			booking.setSendMail(true);
-			
+
 			bookingDao.save(booking);
-			
+
 		}
-		
+
 		Borrowing returnBorrowing = borrowingDao.save(borrowingReturn);
-		
+
 		return new ResponseEntity<Borrowing>(returnBorrowing, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("emprunt/{id}/delete-borrowing")
 	public Response deleteBorrowing(@PathVariable int id) {
-		
+
 		Optional<Borrowing> borrowing = borrowingDao.findById(id);
-		
-		if(!borrowing.isPresent()) throw new BorrowingNotFoundException("Aucun prêt n'a été retrouvé.");
-		
+
+		if (!borrowing.isPresent())
+			throw new BorrowingNotFoundException("Aucun prêt n'a été retrouvé.");
+
 		Borrowing borrowingDelete = borrowing.get();
-		
+
 		String borrowingD = borrowingDelete.toString();
-		
+
 		borrowingDao.delete(borrowingDelete);
-		
-		log.info("Suppression de l'emprunt : " + borrowingD );
-		
+
+		log.info("Suppression de l'emprunt : " + borrowingD);
+
 		return Response.status(200).entity("Borrowing is deleted").build();
 	}
-	
+
 }
